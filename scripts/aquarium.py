23fe5a5c312911c07304773ba44c6bc4dc1ad6b9
MartinTHIBAUD29 <martinthibaud29@gmail.com>
2026-04-12 22:27:37 -0400

    fonction to calculate center of mass

--- scripts/aquarium.py
+++ scripts/aquarium.py
@@ -10,18 +10,19 @@ class Aquarium():
             self.fishes.append(fish.Fish(i, fish_position_x, fish_position_y))
 
     def update_fishes_neighborhood(self):        
-        for fish in self.fishes: 
-            fish.neighbors = []
+        for current_fish in self.fishes: 
+            current_fish.neighbors = []
             for other in self.fishes:
-                if fish.distance_to(other) < fish.field_of_view:
-                    fish.neighbors.append(other.identification)
+                if current_fish.distance_to(other) < current_fish.field_of_view:
+                    current_fish.neighbors.append(other)
      
     def update_fishes_position(self):
         self.update_fishes_neighborhood()
         for fish in self.fishes:
             fish.update_position()
+            
 
-    def create_new_fish(self):
+    def add_new_fish(self):
         fish_position_x = np.random.rand() * world_parameters.SCREEN_WIDTH
         fish_position_y = np.random.rand() * world_parameters.SCREEN_HEIGHT
         self.fishes.append(fish.Fish(len(self.fishes), fish_position_x, fish_position_y))
--- scripts/fish.py
+++ scripts/fish.py
@@ -5,9 +5,9 @@ class Fish:
     def __init__(self, identification, position_x, position_y):
         self.identification = identification
         self.position_x,  self.position_y = position_x, position_y
-        self.speed_x, self.speed_y = np.random.rand() - 0.5, np.random.rand() - 0.5
+        self.speed_x, self.speed_y = np.random.rand() - 0.5 , np.random.rand() - 0.5 
         self.neighbors = []
-        self.field_of_view = np.random.rand() + 0.5 * 30  # distance within which the fish can see other fishes
+        self.field_of_view = (np.random.rand() + 0.5) * 100 
 
     def distance_to(self, fish):
         distance = np.sqrt(
@@ -15,9 +15,34 @@ class Fish:
                 ((self.position_y - fish.position_y)**2))
         return distance
     
+
+    def calculate_center_of_mass_neighbors(self):
+
+        if len(self.neighbors) == 0:
+            return 
+        
+        center_of_mass_x = 0
+        center_of_mass_y = 0
+        for neighbor in self.neighbors:
+            center_of_mass_x += neighbor.position_x
+            center_of_mass_y += neighbor.position_y
+        center_of_mass_x /= len(self.neighbors)
+        center_of_mass_y /= len(self.neighbors)
+
+        return center_of_mass_x, center_of_mass_y
+
+
+    def concentration_rule(self):
+        center_of_mass = self.calculate_center_of_mass_neighbors()
+
+
     def calculate_speed(self):
-        pass
+        speed_contration_rule_x, speed_contration_rule_y = self.concentration_rule()
+        self.speed_x += speed_contration_rule_x
+        self.speed_y += speed_contration_rule_y
+
 
     def update_position(self):
+        self.calculate_speed()
         self.position_x += self.speed_x
         self.position_y += self.speed_y
