b6f4beb4f90d61fb8479867f0c95675ba4e6088a
MartinTHIBAUD29 <martinthibaud29@gmail.com>
2026-04-16 00:11:01 -0400

    moving neighborhood calculation to spatial_grid V2

--- scripts/spatial_grid.py
+++ scripts/spatial_grid.py
@@ -15,7 +15,25 @@ class SpatialGrid():
         self.size_of_column = world_parameters.SCREEN_WIDTH / self.number_of_column
         self.size_of_row = world_parameters.SCREEN_HEIGHT / self.number_of_row
 
+    def update_grid(self, fishes, food):
+        pass
 
+    def find_fish_neighbors(self, fish, fishes):
+        fish.neighbors = []
+        for other in fishes:
+            if fish.distance_to(other.position_x, other.position_y) < fish.field_of_view:
+                fish.neighbors.append(other)
+
+    def find_food_in_sight(self, fish, foods):
+        fish.food_in_sight = {}
+        food_to_remove = []
+        for food in foods:
+            distance_to_food = fish.distance_to(food.position_x, food.position_y)
+            if distance_to_food < world_parameters.FOOD_SIZE:
+                food_to_remove.append(food)
+            elif distance_to_food < food.range_of_detection:
+                    fish.food_in_sight[food] = distance_to_food
+        return food_to_remove
 
 
 
